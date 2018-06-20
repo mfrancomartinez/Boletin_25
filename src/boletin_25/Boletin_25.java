88b4f4d3eeca265b20eb6342c386c6fc8c152c9e
Marcos <Marcos@192.168.1.5>
2018-06-20 12:53:01 +0200

    25

---
+++ src/boletin_25/Boletin_25.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_25;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Boletin_25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Bonoloto ex = new Bonoloto();
+    ex.setVisible(true);
+    //Introduce los números a elección en los jTextField7-12
+    }
+    
+}
